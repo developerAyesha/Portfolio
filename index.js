4a02022cca28c86cd8ddfea42829221ed28c631c
ayesha <ayeshaakh234@gmail.com>
2024-05-26 02:58:15 -0700

    portfolio website

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const typed = new Typed('text', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
+  
